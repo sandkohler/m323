9206e564397a3f6a20a84db448af51de3053e927
Sandro <sandro.kohler@edu.tbz.ch>
2025-05-19 09:05:02 +0200

    :apple: add Aufgabe 2

---
+++ 03_PureFunctions/2.js
@@ -0,0 +1,29 @@
+let cartItems = [];
+
+function addToCart(cartItems, item) {
+    return [...cartItems, item];
+}
+
+let cart = [];
+console.log(cart = addToCart(cart, 'Apple'));  // Ausgabe: ['Apple']
+console.log(cart = addToCart(cart, 'Banana')); // Ausgabe: ['Apple', 'Banana']
+console.log(cart = addToCart(cart, 'Orange')); // Ausgabe: ['Apple', 'Banana', 'Orange']
+
+
+
+// Nicht mehr wirklich random, dafür pure
+function multiplyWithRandom(number, randomValue) {
+    return number * randomValue;
+}
+
+console.log(multiplyWithRandom(5, 3)); // Ausgabe: 15
+console.log(multiplyWithRandom(10, 8)); // Ausgabe: 80
+
+
+
+// Methode printed halt nichts mehr ist dafür aber pure
+function printAndReturnString(str) {
+    return str;      // Rückgabe der Zeichenkette
+}
+
+console.log(printAndReturnString("Hello")); // Ausgabe: Hello
